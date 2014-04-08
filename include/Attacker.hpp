92e315e5072b8db76c2b27af39e2004643f4e8ff
Giulio Trimarco <gtrimarco@digisoft.tv>
2014-04-08 11:10:46 +0100

    Update file to correct version

--- include/Attacker.hpp
+++ include/Attacker.hpp
@@ -1,7 +1,9 @@
-class Attacker {
+class Attacker : public Persistent {
 public :
 	float power; // hit points given
 
 	Attacker(float power);
 	void attack(Actor *owner, Actor *target);
+	void load(TCODZip &zip);
+	void save(TCODZip &zip);
 };
