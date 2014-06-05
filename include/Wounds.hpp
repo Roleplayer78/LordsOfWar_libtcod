ecd1e88ead919759381a2832a3ba60918cd5f699
Giulio Trimarco <gtrimarco@digisoft.tv>
2014-06-05 16:56:15 +0100

    Added (not compiling for now) wounds tablewq!

--- include/Wounds.hpp
+++ include/Wounds.hpp
@@ -1,6 +1,16 @@
 #ifndef WOUNDS_HPP
 #define WOUNDS_HPP
 
+/******************************************************************************
+ *  Wounds constants used to compute other values
+ *
+ *
+ * ***************************************************************************/
+const unsigned short WOUND_MOD_BASE = 5;        // Direct odifier to %
+const unsigned short WOUND_REC_BASE_TIME = 8;   // Hours needed for reducing wound by -1
+const unsigned bool  WOUND_ROUND_UP = true;     // If a wound of less than full WOUND_MOD_BASE affect non % values
+
+// ---------------------------------------------------------------------------
 
 typedef enum {
     WOUND_LEVEL_NONE            = 0,
@@ -21,13 +31,15 @@ typedef enum {
     WOUND_TYPE_UNKNOWN
 } eWoundType;
 
+
 typedef struct {
     eWoundType      WoundType;
     eWoundLevel     WoundLevel;
     short           WoundMod;     // Skill penalty associated with wound
-    short           RecoveryTime; // Time for reducing penalty 1 point
+    unsigned short  RecoveryTime; // Time for roll and try reduce penalty 1 point (in hours)
 } tWoundDef;
 
+extern tWoundDef WoundsTable[];
 
 #endif // WOUNDS_HPP
 
