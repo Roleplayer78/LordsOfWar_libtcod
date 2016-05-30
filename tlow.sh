1aa4276b79413334da88991724a3d1b3b842394f
Roleplayer78 <roleplayingutilitymail@gmail.com>
2016-05-30 21:14:16 +0100

    Added symlink creation for libtcod shared libs

--- tlow.sh
+++ tlow.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libtcod
+if [ ! -h ./libtcod/libtcod.so.1 ]; then
+    ln -s libtcod.so ./libtcod/libtcod.so.1
+fi
+if [ ! -h ./libtcod/libtcodxx.so.1 ]; then
+    ln -s libtcodxx.so ./libtcod/libtcodxx.so.1
+fi
+if [ ! -h ./libtcod/libtcodgui.so.1 ]; then
+    ln -s libtcodgui.so ./libtcod/libtcodgui.so.1
+fi
 
-./build/source/tlow
+
+./source/tlow
